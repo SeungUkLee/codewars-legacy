198495277b84936ad9be088bf147fce794f289a4
SeungUkLee <lsy931106@gmail.com>
2018-03-15 00:30:22 +0900

    solve create_phone_number using java

---
+++ Create_Phone_Number/java/Create_Phone_Number.java
@@ -0,0 +1,31 @@
+/* My Solution */
+public class Kata {
+  public static String createPhoneNumber(int[] numbers) {
+    String res = "";
+    for (int i=0; i<numbers.length; i++) {
+      if (i == 0) {
+          res += "(";
+      } else if (i == 3) {
+          res += ") ";
+      } else if (i == 6) {
+          res += "-";
+      }
+      res += numbers[i];
+    }
+    return res;
+  }
+}
+
+/* Best Pratice */
+public class Kata {
+  public static String createPhoneNumber(int[] numbers) {
+    return String.format("(%d%d%d) %d%d%d-%d%d%d%d",numbers[0],numbers[1],numbers[2],numbers[3],numbers[4],numbers[5],numbers[6],numbers[7],numbers[8],numbers[9]);
+  }
+}
+
+/* Clever */
+public class Kata {
+	  public static String createPhoneNumber(int[] numbers) {
+		      return String.format("(%d%d%d) %d%d%d-%d%d%d%d", java.util.stream.IntStream.of(numbers).boxed().toArray());
+		        }
+	  }
---
+++ README.md
@@ -0,0 +1,4 @@
+# Codewars
+
+https://codewars.com 문제를 풀고 기록하고 정리하는 저장소입니다.
+
